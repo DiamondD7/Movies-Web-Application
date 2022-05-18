3fddcaf50f718351f55942e76ef0b8b02fa5f373
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-05-18 15:00:08 +1200

    Added Model and .cshtml (client side)

---
+++ MoviesWeb/Models/MovieModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesWeb.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public int MoviePrice { get; set; }
+        public string ReleasedDate { get; set; } = DateTime.Now.ToShortDateString();
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        
+    }
+}
