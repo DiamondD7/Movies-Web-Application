53cecff6348136223f79115b84a79ba857471392
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-05-31 00:21:30 +1200

    finishing the overlay effect

--- MoviesWeb/wwwroot/js/EntertainmentScript.js
+++ MoviesWeb/wwwroot/js/EntertainmentScript.js
@@ -1,8 +1,10 @@
 ﻿
 
 const modal = document.querySelector('.genre-modal');
+const overlay = document.querySelector('.overlay');
 
 document.querySelector('.genre').addEventListener('click', function () {
 
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 })
