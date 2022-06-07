277419ec59448d6ef43c75e6a977942ed99751b9
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-07 14:48:05 +1200

    made a form button submit action where the user can get movies by choosing a  genre

--- MoviesWeb/Controllers/EntertainmentController.cs
+++ MoviesWeb/Controllers/EntertainmentController.cs
@@ -16,13 +16,58 @@ namespace MoviesWeb.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(MovieModel movies, string button)
         {
+            if (button == "Horror")
+            {
+                movies.MovieType = "Horror";
+            }
+            else if(button == "Action")
+            {
+                movies.MovieType = "Action";
+            }
+            else if (button == "Adventure")
+            {
+                movies.MovieType = "Adventure";
+            }
+            else if (button == "Comedy")
+            {
+                movies.MovieType = "Comedy";
+            }
+            else if (button == "Thriller")
+            {
+                movies.MovieType = "Thriller";
+            }
+            else if (button == "Sci-Fi")
+            {
+                movies.MovieType = "Sci-Fi";
+            }
+            else if (button == "Documentary")
+            {
+                movies.MovieType = "Documentary";
+            }
+            else if (button == "Bio")
+            {
+                movies.MovieType = "Bio";
+            }
             IEnumerable<MovieModel> movie = _context.Order;
-            var genreSelected = movie.Where(x => x.MovieType == "Horror");
+            var genreSelected = movie.Where(c => c.MovieType == movies.MovieType);
             return View(genreSelected);
         }
 
-    
+
+/*        [HttpPost]
+        public IActionResult Index(MovieModel movieModel, string button, string idk)
+        {
+
+            if (button == "Horror")
+            {
+                movieModel.MovieType = "Horror";
+            }
+            return View(movieModel.MovieType);
+
+        }*/
+
+
     }
 }
--- MoviesWeb/Models/HorrorMovies.cs
+++ MoviesWeb/Models/HorrorMovies.cs
@@ -7,6 +7,13 @@ namespace MoviesWeb.Models
 {
     public class HorrorMovies
     {
-        public string MType { get; set; }
+        public List<MovieModel> Sample()
+        {
+            List<MovieModel> movieModels = new List<MovieModel>();
+            movieModels.Add(new MovieModel { MovieType = "Horror" });
+            movieModels.Add(new MovieModel { MovieType = "Action" });
+            movieModels.Add(new MovieModel { MovieType = "Thriller" });
+            return movieModels;
+        }
     }
 }
